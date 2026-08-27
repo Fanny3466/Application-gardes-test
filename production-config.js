9ee5e16314718f30fad4925b5c39f56cec42fdd9
Fanny3466 <choulet.fanny70@gmail.com>
2026-08-27 12:13:21 +0200

    Add files via upload

--- production-config.js
+++ production-config.js
@@ -1,5 +1,5 @@
 /*
- * Généré par setup.html V2.3.3 le 24/08/2026 12:54:13
+ * Généré par setup.html V2.3.3 le 27/08/2026 12:12:17
  * Ne contient aucun client secret.
  */
 window.GARDES_PRODUCTION = {
@@ -7,8 +7,8 @@ window.GARDES_PRODUCTION = {
   tenantId: "b5849dc4-c17a-4f94-9680-38759588959e",
   clientId: "e652d620-c4b0-4feb-a426-193ef645c807",
   driveId: "b!XmIPovBME0eFYmr5OZoATsb_Wwz89txFpfAvdlRCfZRG7MpmD7R7S4s8xHYXaiUJ",
-  itemId: "01FQPFEMCNOZKPN4VI2ZCZZBWMBMUBR546",
-  workbookName: "FEUILLES DE GARDES (test automatisation).xlsm",
+  itemId: "01FQPFEMDEU7QACLVQGRDL6H2VYAFUPX3U",
+  workbookName: "FEUILLES DE GARDES.xlsm",
   adminEmails: ["Fanny.CHOULET@sdis34.fr"],
   bootstrapAdminAgentCode: "CFa",
   maxExcelSyncAgeMinutes: 5
